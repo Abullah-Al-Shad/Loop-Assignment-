133f341da57ee680e2250a570b4c85a49416ee2e
Shad <60463469+Abullah-Al-Shad@users.noreply.github.com>
2021-11-25 15:59:25 +0600

    Add files via upload

---
+++ Abdullah Al Shad -(Loop Assignment )/4.php
@@ -0,0 +1,49 @@
+<?php
+
+
+// Multiplication Table of- 2
+
+for ($x = 1; $x <= 20; $x++){
+    if($x % 2 == 0){
+        $y = $x/2;
+        echo "2*$y=$x <br>";
+    
+    }
+}
+echo "<hr>";
+
+
+// Multiplication Table of- 3
+
+for ($x = 1; $x <= 30; $x++){
+    if($x % 3 == 0){
+        $y = $x/3;
+        echo "3*$y=$x <br>";
+    
+    }
+}
+echo "<hr>";
+
+
+// Multiplication Table of- 4
+
+for ($x = 1; $x <= 40; $x++){
+    if($x % 4 == 0){
+        $y = $x/4;
+        echo "4*$y=$x <br>";
+    
+    }
+}
+echo "<hr>";
+
+
+// Multiplication Table of- 5
+
+for ($x = 1; $x <= 50; $x++){
+    if($x % 5 == 0){
+        $y = $x/5;
+        echo "5*$y=$x <br>";
+    
+    }
+}
+echo "<hr>";
