c6d3f1c98bc02e092ae5fea86e1c5ca1275c7877
Shad <60463469+Abullah-Al-Shad@users.noreply.github.com>
2021-11-26 00:14:55 +0600

    Add files via upload

---
+++ Abdullah Al Shad -(Loop Assignment )/Multiplication Table.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multiplication Table</title>
+</head>
+<html>
+    <body>
+        <form action="Multiplication Table.php" method="post">
+            Enter a number:
+            <input type="number_format" name="x" >
+            <input type="submit" value="Times Table">
+        </form>
+        
+        <?php
+        $x = $_POST['x'] ?? 1;
+        echo "<h1>Multiplication Table of = $x</h1>";
+        for ($y = 1; $y <= $x*10; $y++){
+            if($y % $x == 0){
+                $z = $y/$x;
+                echo "<h3>$x*$z=$y</h3> <hr>";
+            
+            }
+        }
+        ?>
+    
+    </body>
+</html>
+</html>
